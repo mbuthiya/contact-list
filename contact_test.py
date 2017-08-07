1df39a6808116b0a61dfea3497e028281836c1fb
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:06:32 +0300

    Create first test for checking if the contact is being instanciated

--- contact_test.py
+++ contact_test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-from contact import Contact #Importing the contact class
+from contact import Contact # Importing the contact class
 
 class TestContact(unittest.TestCase):
     '''
@@ -8,6 +8,13 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+
+    def test_init(self):
+        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com")
+        self.assertEqual(self.new_contact.first_name,"James")
+        self.assertEqual(self.new_contact.last_name,"Muriuki")
+        self.assertEqual(self.new_contact.phone_number,"0712345678")
+        self.assertEqual(self.new_contact.email,"james@ms.com")
+
 if __name__ == '__main__':
     unittest.main() #Statement to compile the test cases and run them all
