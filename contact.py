9c9a166f614df05981c99bd5480e8a0f9b4657c6
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:46:03 +0300

    Add delete_contact method
    created a method that removes the contact object in the contact list

--- contact.py
+++ contact.py
@@ -7,6 +7,7 @@ class Contact:
 
 
     def __init__(self,first_name,last_name,phone_number,email):
+
         '''
         __init__ method that helps us create properties for our objects.
 
@@ -16,13 +17,26 @@ class Contact:
             phone_number: New contact phone number.
             email : New contact email address.
         '''
+
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
 
+
     def save_contact(self):
+
         '''
         save_contact method saves contact objects into contact_list
         '''
+
         Contact.contact_list.append(self)
+
+
+    def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
