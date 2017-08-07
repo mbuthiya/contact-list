b269129f4f5cabf144cfe381aff915cd0bc3802f
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 10:34:37 +0300

    Added test case for initialization method

--- fav-contact_test.py
+++ fav-contact_test.py
@@ -1,8 +1,28 @@
 import unittest
-from fav-contact import FavouriteContact
+from fav_contact import FavouriteContact
 
 class TestFavouriteContact(unittest.TestCase):
+    '''
+    Test class to test behaviours of the Favourite contact class
+    '''
+
+    def setUp(self):
+
+        self.new_fav_contact = FavouriteContact("James","Muriuki","0712345678","james@ms.com")
+
+    def test_init(self):
+        '''
+        Method to test  if new contact is instanciated correctly
+        '''
+
+        self.assertEqual(self.new_fav_contact.first_name,"James")
+        self.assertEqual(self.new_fav_contact.last_name,"Muriuki")
+        self.assertEqual(self.new_fav_contact.phone_number,"0712345678")
+        self.assertEqual(self.new_fav_contact.email,"james@ms.com")
+
     
+
+
 
 if __name__ == "__main__":
     unittest.main()
