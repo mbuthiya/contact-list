f19b461a7bf720e549fef810f57ca34d3f1cfa74
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:34:57 +0300

    Create new save test case created a new save test case to see if our  application can save multiple contact objects

--- contact_test.py
+++ contact_test.py
@@ -34,5 +34,17 @@ class TestContact(unittest.TestCase):
         self.new_contact.save_contact() # saving the new contact
         self.assertEqual(len(Contact.contact_list),1)
 
+    def test_save_multiple_contact(self):
+        '''
+        test_save_multiple_contact to check if we can save multiple contact
+        objects to our contact_list
+        '''
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
+        test_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list),2)
+
+
+
 if __name__ == '__main__':
     unittest.main() #Statement to compile the test cases and run them all
