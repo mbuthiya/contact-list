ccafd111277f7fa91bea8bd2f33438fa4050a8a2
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 14:01:37 +0300

    Create new_contact function
    Function to create a new contact object

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 #!virtual/bin/python3.6
+
+from contact import Contact
+
+def create_contact(fname,lname,phone,email):
+    '''
+    Function to create a new contact
+    '''
+    return new_contact = Contact(fname,lname,phone,email)
