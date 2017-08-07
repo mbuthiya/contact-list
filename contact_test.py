ac0e97fc2492af7764172458e0a027ecb1de4fa4
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:01:06 +0300

    Set up Contact test class

---
+++ contact_test.py
@@ -0,0 +1,13 @@
+import unittest # Importing the unittest module
+from contact import Contact #Importing the contact class
+
+class TestContact(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
+if __name__ == '__main__':
+    unittest.main() #Statement to compile the test cases and run them all
