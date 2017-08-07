3ad4044bf47b6896fd1bdf6188c9bfe82f620b89
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 13:16:48 +0300

    Created save_fav_contact method
    Implemented the save contact method to save a new favourite
    contact to a list

--- fav_contact.py
+++ fav_contact.py
@@ -9,6 +9,8 @@ class FavouriteContact(Contact):
         Contact: The Contact class which is the parent Class and where we inherit the class behaviours
     '''
 
+    favourite_contacts = []
+
     def __init__(self,first_name,last_name,phone_number,email,birthday):
         '''
         __init__ method helping us create properties for our objects
@@ -16,3 +18,8 @@ class FavouriteContact(Contact):
         '''
         Contact.__init__(self,first_name,last_name,phone_number,email)
         self.birthday = birthday
+
+
+
+    def save_fav_contact(self):
+        FavouriteContact.favourite_contacts.append(self)
