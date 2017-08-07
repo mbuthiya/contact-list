07990be5d5412450af45141753f42f5f57a2877e
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 11:21:58 +0300

    Create Favourite init method
    Created a favourite contact init method and passed in the object attributes

---
+++ fav_contact.py
@@ -0,0 +1,18 @@
+from contact import Contact
+
+class FavouriteContact(Contact):
+
+    '''
+    FavouriteContact class that creates contact objects
+
+    Args:
+        Contact: The Contact class which is the parent Class and where we inherit the class behaviours
+    '''
+
+    def __init__(self,first_name,last_name,phone_number,email,birthday):
+        '''
+        __init__ method helping us create properties for our objects
+
+        '''
+        Contact.__init__(self,first_name,last_name,phone_number,email)
+        self.birthday = birthday
