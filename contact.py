c539d29a85e722f1951e2ccf3f2a6f6d350ae393
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:14:33 +0300

    Create __init__ method in the contact class Created the __init__ method in the contact class and set up all the object properties

--- contact.py
+++ contact.py
@@ -1,2 +1,18 @@
 class Contact:
-    pass
+    """
+    Contact class that creates contact objects.
+    """
+    def __init__(self,first_name,last_name,phone_number,email):
+        '''
+        __init__ method that helps us create properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            phone_number: New contact phone number.
+            email : New contact email address.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
