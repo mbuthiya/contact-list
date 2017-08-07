8130a0c7b77530af62c805d3ad5da26b601d1361
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-08-07 04:19:21 +0300

    Create setUp test method
    Created  a set up test method that initializes the class before every test
    case

--- contact_test.py
+++ contact_test.py
@@ -9,8 +9,18 @@ class TestContact(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
-    def test_init(self):
+    def setUp(self):
+        '''
+        Set up method to run before all other test cases
+        '''
         self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com")
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
         self.assertEqual(self.new_contact.first_name,"James")
         self.assertEqual(self.new_contact.last_name,"Muriuki")
         self.assertEqual(self.new_contact.phone_number,"0712345678")
